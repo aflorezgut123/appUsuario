250850a2bf08721c472f8eba36f7cc5a4576f1cd
aflorezgut123 <andersonflorez9402@gmail.com>
2021-11-20 08:27:51 -0500

    Initial commit

---
+++ app/src/main/java/co/org/poli/appusuarios/MainActivity.java
@@ -0,0 +1,43 @@
+package co.org.poli.appusuarios;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottomNavContainer);
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragmentSelected = null;
+                switch (item.getItemId()) {
+                    case R.id.navCreateUser:
+                        fragmentSelected = new AddUsersFragment();
+                        break;
+                    case R.id.navUserList:
+                        fragmentSelected = new UserlistFragment();
+                        break;
+                    case R.id.navInfo:
+                        fragmentSelected = new InfoFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragmentSelected).commit();
+                return true;
+            }
+        });
+    }
+}
