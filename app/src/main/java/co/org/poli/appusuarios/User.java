f178ca1a72a99413bec8a945239a8240ce12a1ae
aflorezgut123 <andersonflorez9402@gmail.com>
2021-11-20 08:56:02 -0500

    adicion listav1 y clase user

---
+++ app/src/main/java/co/org/poli/appusuarios/User.java
@@ -0,0 +1,49 @@
+package co.org.poli.appusuarios;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String name;
+    private String genre;
+    private String description;
+    private String urlImage;
+
+    public User(String name, String genre, String description, String urlImage) {
+        this.name = name;
+        this.genre = genre;
+        this.description = description;
+        this.urlImage = urlImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
