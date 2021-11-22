74a3ab94d611dfeaab0fbe106c04c9b9dc13d4e6
Luis Alberto Roa Huertas <luis.roa@avaldigitallabs.com>
2021-11-21 19:52:06 -0500

    update register
    Se hicieron ajustes de registro y temas visuales de diseño

--- app/src/main/java/co/org/poli/appusuarios/AddUsersFragment.java
+++ app/src/main/java/co/org/poli/appusuarios/AddUsersFragment.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,6 +28,9 @@ public class AddUsersFragment extends Fragment {
     private static final String TAG = "MyActivity";
     View view;
     User user;
+    EditText txtName;
+    EditText txtDescription;
+    EditText txtURLimage;
 
     @Nullable
     @Override
@@ -33,20 +39,19 @@ public class AddUsersFragment extends Fragment {
         Spinner opciones = view.findViewById(R.id.cmbGenero);
         Button btnGuardar = view.findViewById(R.id.btnGuardar);
 
-        btnGuardar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Guardar();
-            }
-        });
+        btnGuardar.setOnClickListener(v -> Guardar());
+
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.opciones, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         opciones.setAdapter(adapter);
         return view;
+
     }
 
     public void Guardar(){
+        txtName = view.findViewById(R.id.idTextName);
+        txtName.setText("");
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Map<String, Object> user = new HashMap<>();
         user.put("first", "Ada");
@@ -54,19 +59,21 @@ public class AddUsersFragment extends Fragment {
         user.put("born", 1815);
 
 // Add a new document with a generated ID
-        db.collection("usuario")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
+        Toast.makeText(getActivity(),"Registro Enviado",Toast.LENGTH_SHORT).show();
+//        db.collection("usuario")
+//                .add(user)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+//                    }
+//                });
     }
+
 }
