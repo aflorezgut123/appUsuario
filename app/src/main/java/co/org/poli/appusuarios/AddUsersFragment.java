87f7bfca5f24f84a96c6fa9b89901cc6c033cc6c
Luis Alberto Roa Huertas <luis.roa@avaldigitallabs.com>
2021-11-28 12:11:18 -0500

    Add get from jsonplaceholder
    Add from api https://jsonplaceholder.typicode.com/users/ donde se consulta los datos del json

--- app/src/main/java/co/org/poli/appusuarios/AddUsersFragment.java
+++ app/src/main/java/co/org/poli/appusuarios/AddUsersFragment.java
@@ -16,16 +16,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class AddUsersFragment extends Fragment {
-    private static final String TAG = "MyActivity";
+
+    private EditText etUserId, etUsername, etEmail, etName;
+    private Button btnSendAction;
+    private final static String API_URL = "https://jsonplaceholder.typicode.com/users/";
+
+
     View view;
     User user;
     EditText txtName;
@@ -36,44 +49,62 @@ public class AddUsersFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.create_user_fragment, container, false);
-        Spinner opciones = view.findViewById(R.id.cmbGenero);
-        Button btnGuardar = view.findViewById(R.id.btnGuardar);
+        etUserId = view.findViewById(R.id.etUserID);
+        etUsername = view.findViewById(R.id.etUserName);
+        etEmail = view.findViewById(R.id.etEMail);
+        etName = view.findViewById(R.id.etName);
+        btnSendAction = view.findViewById(R.id.btnSendAction);
 
-        btnGuardar.setOnClickListener(v -> Guardar());
+        btnSendAction.setOnClickListener(v -> makeGetRequest());
 
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.opciones, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        opciones.setAdapter(adapter);
         return view;
 
     }
 
-    public void Guardar(){
-        txtName = view.findViewById(R.id.idTextName);
-        txtName.setText("");
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
 
-// Add a new document with a generated ID
-        Toast.makeText(getActivity(),"Registro Enviado",Toast.LENGTH_SHORT).show();
-//        db.collection("usuario")
-//                .add(user)
-//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                    @Override
-//                    public void onSuccess(DocumentReference documentReference) {
-//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.w(TAG, "Error adding document", e);
-//                    }
-//                });
+    private void makeGetRequest() {
+        String numApi = etUserId.getText().toString();
+        if (numApi.equals("1") || numApi.equals("2") ||
+                numApi.equals("3") || numApi.equals("4") ||
+                numApi.equals("5") || numApi.equals("6") ||
+                numApi.equals("7") || numApi.equals("8") ||
+                numApi.equals("9") || numApi.equals("10")) {
+
+            StringRequest strRequest = new StringRequest(Request.Method.GET, API_URL + numApi, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject responseObject = new JSONObject(response);
+                        String userId = responseObject.getString("id");
+                        String userName = responseObject.getString("username");
+                        String name = responseObject.getString("name");
+                        String email = responseObject.getString("email");
+
+                        etUserId.setText(userId);
+                        etUsername.setText(userName);
+                        etName.setText(name);
+                        etEmail.setText(email);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+
+                    }
+                }
+            },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("[RESQUEST ERROR]", error.getMessage());
+                        }
+                    }
+            );
+            Volley.newRequestQueue(this.view.getContext()).add(strRequest);
+        } else {
+            Toast toast1 =
+//                    Toast.makeText(getApplicationContext(),
+                    Toast.makeText(this.view.getContext(),
+                            "Por favor ingrese un valor entre 1 a 10", Toast.LENGTH_SHORT);
+            toast1.show();
+        }
     }
 
 }
